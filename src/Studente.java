f4dcf626f238738f98a9bc2ea607db64627287bb
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-12-20 09:32:04 +0100

    Attributi, costruttore e metodi set e get della classe Studente.

--- src/Studente.java
+++ src/Studente.java
@@ -1,3 +1,20 @@
 public class Studente {
-
+    private String nome;
+    private double punteggioTot;
+    public Studente(){
+        nome="";
+        punteggioTot=0;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setPunteggioTot(double punteggioTot){
+        this.punteggioTot=punteggioTot;
+    }
+    public double getPunteggioTot(){
+        return punteggioTot;
+    }
 }
